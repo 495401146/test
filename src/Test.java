65eebfe8523b79d4fe75adfeae2d36c687fd905a
hechangzhi <495401146@qq.com>
2017-06-24 00:57:11 +0800

    idea测试

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,26 @@
+
+
+
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Random;
+
 /**
  * Created by 49540 on 2017/6/16.
  */
 public class Test {
-    public static void main(String[] args)
+    public static void print(int index,Object obj)
     {
-        System.out.println("wo shi yi ge");
+        System.out.println(String.format("{%d},%s",index,obj));
     }
+    public static void demoCollection()
+    {
+        ArrayList list = new ArrayList();
+        LinkedList list1 = new LinkedList();
+    }
+    public static void main(String[] args) {
+        Random random = new Random();
+        System.out.println(random.nextInt());
+     }
 }
